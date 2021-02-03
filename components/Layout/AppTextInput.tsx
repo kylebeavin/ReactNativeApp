d03aff1690824404dfaf5f611197e2d63a3f30e9
Kyle Beavin <kyle.beavin@tcmcllc.com>
2021-02-03 16:07:57 -0500

    Custom AppTextInput Component with Validation, Updated CreateAccountModal to use AppTextInputs. -- KB

---
+++ components/Layout/AppTextInput.tsx
@@ -0,0 +1,93 @@
+import React, {useRef, useState} from 'react';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
+import Colors from '../../constants/Colors';
+
+interface Props {
+  label: string;
+  name: string;
+  value: string;
+  onChange: (value: string) => void;
+  validations: any;
+  errors: any;
+  setErrors: any;
+  multiline?: boolean;
+}
+
+const AppTextInput: React.FC<Props> = ({label,name,value,onChange,validations,errors,setErrors,multiline}) => {
+  const [focused, setFocused] = useState(false);
+  const ref = useRef(null);
+
+  const validate = (validations: any) => {
+    setErrors((prev:any) => ({
+      ...prev,
+      [name]: validations
+        .map((errorsFor: any) => errorsFor(value))
+        .filter((errorMsg: any) => errorMsg.length > 0),
+    }));
+  };
+
+  return (
+    <View style={styles.fieldContainer}>
+      <Text style={styles.text} onPress={() => ref.current!.focus()}>{label}</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.textInput}
+        ref={ref}
+        value={value}
+        onChange={(e) => onChange(e.nativeEvent.text)}
+        blurOnSubmit={false}
+        onFocus={() => setFocused(true)}
+        onBlur={() => {
+          setFocused(false);
+          validate(validations);
+        }}
+
+        multiline={!multiline ? false : true}
+      />
+
+      <View style={[focused ? styles.isFocused : null, multiline ? {borderBottomColor: 'transparent'}: null]}></View>
+      <View style={errors.length > 0 ? styles.errorStyle : null}></View>
+
+      <View style={errors.length > 0 ? {opacity: 1} : {opacity: 0}}>
+        <Text style={{color: Colors.SMT_Primary_1}}>{errors.join(', ')}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  fieldContainer: {
+    //marginBottom: 10,
+  },
+  isFocused: {
+    position: 'absolute',
+    top: 21,
+    height: '50%',
+    width: '100%',
+    borderColor: Colors.SMT_Secondary_2_Light_2,
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+  errorStyle: {
+    position: 'absolute',
+    bottom: 21.5,
+    alignSelf: 'center',
+    width: '98.6%',
+    borderBottomColor: Colors.SMT_Primary_1,
+    borderBottomWidth: 2,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: Colors.SMT_Secondary_1,
+  },
+  textInput: {
+    paddingLeft: 15,
+    paddingVertical: 5,
+    borderColor: Colors.SMT_Secondary_1_Light_1,
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+});
+
+export default AppTextInput;
--- screens/Modal/Creates/CreateAccountModal.tsx
+++ screens/Modal/Creates/CreateAccountModal.tsx
@@ -1,53 +1,64 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {useContext, useRef, useState} from 'react';
-import {StyleSheet, View, Text, TextInput, ScrollView} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {StyleSheet, View, ScrollView} from 'react-native';
+import AppTextInput from '../../../components/Layout/AppTextInput';
 
 import Colors from '../../../constants/Colors';
 import Configs from '../../../constants/Configs';
 import Layout from '../../../constants/Layout';
-import useAsyncStorage from '../../../hooks/useAsyncStorage';
 import AppContext from '../../../providers/AppContext';
 import {ToastContext} from '../../../providers/ToastProvider';
-import { IValidator, Validator } from '../../../types';
 import {Account} from '../../../types/crm';
-import {
-  getRequestHeadersAsync,
-  isSuccessStatusCode,
-  isValidEmail,
-} from '../../../utils/Helpers';
+import {isSuccessStatusCode} from '../../../utils/Helpers';
 import ModalButtons from '../ModalButtons';
 
-interface Props {}
+const defaultValues = {
+  name: '',
+  email: '',
+  street: '',
+  city: '',
+  state: '',
+  zip: '',
+  notes: ''
+}
 
-const CreateAccountModal: React.FC<Props> = () => {
+const defaultErrors = {
+  name: [],
+  email: [],
+  street: [],
+  city: [],
+  state: [],
+  zip: [],
+  notes: []
+}
+
+const isRequired = (val: string) => {
+  return val.length > 0 ? '' : 'Can not be blank';
+};
+
+const isEmail = (val: string) => {
+  const ai = val.indexOf("@");
+  const gdi = val.split("").reduce((acc:any, char: any, i: any) => char === "." ? i : acc, 0);
+  return ai > -1 && gdi > ai ? "" : "Must be an email";
+};
+
+const CreateAccountModal = () => {
   //#region State Variables
   const navigation = useNavigation();
+
   const {grpId, token, id, displayName} = useContext(AppContext);
   const {show} = useContext(ToastContext);
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [street, setStreet] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [zip, setZip] = useState('');
-  const [notes, setNotes] = useState('');
-
-  const [nameValidator, setNameValidator] = useState(new Validator());
-  const [emailValidator, setEmailValidator] = useState(new Validator());
-  const [streetValidator, setStreetValidator] = useState(new Validator());
-  const [cityValidator, setCityValidator] = useState(new Validator());
-  const [stateValidator, setStateValidator] = useState(new Validator());
-  const [zipValidator, setZipValidator] = useState(new Validator());
-  const [notesValidator, setNotesValidator] = useState(new Validator());
-
-  const nameRef = useRef<TextInput>(null);
-  const emailRef = useRef<TextInput>(null);
-  const streetRef = useRef<TextInput>(null);
-  const cityRef = useRef<TextInput>(null);
-  const stateRef = useRef<TextInput>(null);
-  const zipRef = useRef<TextInput>(null);
-  const notesRef = useRef<TextInput>(null);
+  
+  const [values, setValues] = useState(defaultValues);
+  const [errors, setErrors] = useState(defaultErrors);
+  
+  // const nameRef = useRef<TextInput>(null);
+  // const emailRef = useRef<TextInput>(null);
+  // const streetRef = useRef<TextInput>(null);
+  // const cityRef = useRef<TextInput>(null);
+  // const stateRef = useRef<TextInput>(null);
+  // const zipRef = useRef<TextInput>(null);
+  // const notesRef = useRef<TextInput>(null);
   //#endregion
 
   const getFormData = async () => {
@@ -56,14 +67,14 @@ const CreateAccountModal: React.FC<Props> = () => {
       group_id: grpId,
       owner_id: id,
       owner_name: displayName,
-      account_name: name,
-      address_street: street,
-      address_city: city,
-      address_state: state,
-      address_zip: zip,
+      account_name: values.name,
+      address_street: values.street,
+      address_city: values.city,
+      address_state: values.state,
+      address_zip: values.zip,
       created: '',
       demo: '',
-      email: email,
+      email: values.email,
       hauling_contract: false,
       hauling_expiration: '',
       stage: 'Lead',
@@ -74,7 +85,7 @@ const CreateAccountModal: React.FC<Props> = () => {
       national: false,
       referral: false,
       referral_group_id: '',
-      notes: [notes],
+      notes: [values.notes],
       drawerIsVisible: false,
     };
 
@@ -83,9 +94,8 @@ const CreateAccountModal: React.FC<Props> = () => {
 
   const postNewAccount = async () => {
     const account = await getFormData();
-    const validator = validateAccountForm(account);
     
-    if (validator.isValid){
+    if (false){
       await fetch(`${Configs.TCMC_URI}/api/accounts`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json', 'x-access-token': token},
@@ -106,220 +116,132 @@ const CreateAccountModal: React.FC<Props> = () => {
     }
   };
 
-  const validateAccountForm = (account: Account) : IValidator => {
-    let validator : IValidator = new Validator();
-    validator.isVisible = true;
-    let emailObj = isValidEmail(email);
-
-    if (!account) return validator;
-    if (!name || name.trim().length === 0) {
-      setNameValidator({isVisible: true, isValid: false, message: "Name required."})
-    } else {
-      setNameValidator({isValid: true, message: "", isVisible: false});
-    }
-    if (emailObj.isValid) {
-      setEmailValidator({isValid: true, message: emailObj.message, isVisible: false});
-    } else {
-      setEmailValidator({isValid: false, message: emailObj.message, isVisible: true})
-    }
-    if (!street || street.trim().length === 0) {
-      setStreetValidator({isValid: false, message: "Street required.", isVisible: true})
-    } else {
-      setStreetValidator({isValid: true, message: "", isVisible: false})
-    }
-    if (!city || city.trim().length === 0) {
-      setCityValidator({isValid: false, message: "City required.", isVisible: true})
-    } else {
-      setCityValidator({isValid: true, message: "", isVisible: false})
-    }
-    if (!state || state.trim().length === 0) {
-      setStateValidator({isValid: false, message: "State required.", isVisible: true})
-    } else {
-      setStateValidator({isValid: true, message: "", isVisible: false})
-    }    
-    if (!zip || zip.trim().length === 0) {
-      setZipValidator({isValid: false, message: "Zip required.", isVisible: true})
-    } else {
-      setZipValidator({isValid: true, message: "", isVisible: false})
-    }
-
-    if (!nameValidator.isVisible && !emailValidator.isVisible && !streetValidator.isVisible && !cityValidator.isVisible && !stateValidator.isVisible && !zipValidator.isVisible) {
-        validator.isValid = true;
-    }
-
-    return validator;
-  }
-
   return (
     <View>
       <ScrollView style={styles.form}>
+
         {/* Name */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>Name</Text>
-          <TextInput
-            style={nameValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={nameRef}
-            value={name}
-            onChange={(text) => setName(text.nativeEvent.text)}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              if (!name || name.trim().length === 0) {
-                setNameValidator({isVisible: true, isValid: false, message: "Name required."})
-              } else {
-                setNameValidator({isValid: true, message: "", isVisible: false});
-              }
-              return emailRef.current!.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <View style={nameValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {nameValidator.message}
-            </Text>
-          </View>
-        </View>
+        <AppTextInput
+          label="Name"
+          name="name"
+          value={values.name}
+          onChange={(val) => {
+            const name = val;
+            setValues((prev) => ({
+              ...prev,
+              name,
+            }))
+          }}
+          validations={[isRequired]}
+          errors={errors.name}
+          setErrors={setErrors}
+        />
 
-        {/* Email Address */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>Email Address</Text>
-          <TextInput
-            style={emailValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={emailRef}
-            value={email}
-            onChange={(text) => setEmail(text.nativeEvent.text)}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              let test = isValidEmail(email.trim());
-              if (test.isValid) {
-                setEmailValidator({isValid: true, message: test.message, isVisible: false});
-              } else {
-                setEmailValidator({isValid: false, message: test.message, isVisible: true})
-              }
-              return streetRef.current!.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <View style={emailValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {emailValidator.message}
-            </Text>
-          </View>
-        </View>
+        {/* Email */}
+        <AppTextInput
+          label="Email"
+          name="email"
+          value={values.email}
+          onChange={(val) => {
+            const email = val;
+            setValues((prev) => ({
+              ...prev,
+              email,
+            }))
+          }}
+          validations={[isRequired, isEmail]}
+          errors={errors.email}
+          setErrors={setErrors}
+        />
 
-        {/* Street Address */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>Street Address</Text>
-          <TextInput
-            style={streetValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={streetRef}
-            value={street}
-            onChange={(text) => setStreet(text.nativeEvent.text)}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              if (!street || street.trim().length === 0) {
-                setStreetValidator({isValid: false, message: "Street required.", isVisible: true})
-              } else {
-                setStreetValidator({isValid: true, message: "", isVisible: false})
-              }
-              return cityRef.current!.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <View style={streetValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {streetValidator.message}
-            </Text>
-          </View>
-        </View>
+        {/* Street */}
+        <AppTextInput
+          label="Street"
+          name="street"
+          value={values.street}
+          onChange={(val) => {
+            const street = val;
+            setValues((prev) => ({
+              ...prev,
+              street,
+            }))
+          }}
+          validations={[isRequired]}
+          errors={errors.street}
+          setErrors={setErrors}
+        />
 
         {/* City */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>City</Text>
-          <TextInput
-            style={cityValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={cityRef}
-            value={city}
-            onChange={(text) => setCity(text.nativeEvent.text)}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              if (!city || city.trim().length === 0) {
-                setCityValidator({isValid: false, message: "City required.", isVisible: true})
-              } else {
-                setCityValidator({isValid: true, message: "", isVisible: false})
-              }
-              return stateRef.current!.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <View style={cityValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {cityValidator.message}
-            </Text>
-          </View>
-        </View>
+        <AppTextInput
+          label="City"
+          name="city"
+          value={values.city}
+          onChange={(val) => {
+            const city = val;
+            setValues((prev) => ({
+              ...prev,
+              city,
+            }))
+          }}
+          validations={[isRequired]}
+          errors={errors.city}
+          setErrors={setErrors}
+        />
 
         {/* State */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>State</Text>
-          <TextInput
-            style={stateValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={stateRef}
-            value={state}
-            onChange={(text) => setState(text.nativeEvent.text)}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              if (!state || state.trim().length === 0) {
-                setStateValidator({isValid: false, message: "State required.", isVisible: true})
-              } else {
-                setStateValidator({isValid: true, message: "", isVisible: false})
-              }
-              return zipRef.current!.focus();
-            }}
-            blurOnSubmit={false}
-          />
-          <View style={stateValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {stateValidator.message}
-            </Text>
-          </View>
-        </View>
+        <AppTextInput
+          label="State"
+          name="state"
+          value={values.state}
+          onChange={(val) => {
+            const state = val;
+            setValues((prev) => ({
+              ...prev,
+              state,
+            }))
+          }}
+          validations={[isRequired]}
+          errors={errors.state}
+          setErrors={setErrors}
+        />
 
         {/* Zip */}
-        <View style={styles.fieldContainer}>
-          <Text style={styles.text}>Zip</Text>
-          <TextInput
-            style={zipValidator.isVisible ? [styles.textInput, {borderBottomColor: Colors.SMT_Primary_1}] : styles.textInput} 
-            ref={zipRef}
-            value={zip}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              if (!zip || zip.trim().length === 0) {
-                setZipValidator({isValid: false, message: "Zip required.", isVisible: true})
-              } else {
-                setZipValidator({isValid: true, message: "", isVisible: false})
-              }            
-              return notesRef.current!.focus();
-            }}
-            onChange={(text) => setZip(text.nativeEvent.text)}
-            blurOnSubmit={false}
-          />
-          <View style={zipValidator.isVisible ? {opacity: 1} : {opacity: 0}}>
-            <Text style={{color: Colors.SMT_Primary_1}}>
-              {zipValidator.message}
-            </Text>
-          </View>
-        </View>
+        <AppTextInput
+          label="Zip"
+          name="zip"
+          value={values.zip}
+          onChange={(val) => {
+            const zip = val;
+            setValues((prev) => ({
+              ...prev,
+              zip,
+            }))
+          }}
+          validations={[isRequired]}
+          errors={errors.zip}
+          setErrors={setErrors}
+        />
 
         {/* Notes */}
-        <View style={[styles.fieldContainer, {marginBottom: 40}]}>
-          <Text style={styles.text}>Notes</Text>
-          <TextInput
-            style={styles.textInput}
-            ref={notesRef}
-            value={notes}
-            onChange={(text) => setNotes(text.nativeEvent.text)}
-          />
+        <View style={{marginBottom: 40}}>
+        <AppTextInput
+          label="Notes"
+          name="notes"
+          value={values.notes}
+          onChange={(val) => {
+            const notes = val;
+            setValues((prev) => ({
+              ...prev,
+              notes,
+            }))
+          }}
+          validations={[]}
+          errors={errors.notes}
+          setErrors={setErrors}
+          multiline
+        />
         </View>
+
       </ScrollView>
 
       {/* Buttons */}
@@ -335,21 +257,7 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 4,
     backgroundColor: Colors.SMT_Tertiary_1,
-  },
-  fieldContainer: {
-    //marginBottom: 10,
-  },
-  text: {
-    fontWeight: 'bold',
-    color: Colors.SMT_Secondary_1,
-  },
-  textInput: {
-    paddingLeft: 15,
-    paddingVertical: 5,
-    borderColor: Colors.SMT_Secondary_1_Light_1,
-    borderWidth: 2,
-    borderRadius: 3,
-  },
+  }
 });
 
 export default CreateAccountModal;
